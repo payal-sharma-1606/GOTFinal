8047a870038dfaa5f63b4839fe250ac16c4e2a7f
payal <payal2534@gmail.com>
2018-05-14 14:28:54 +0530

    factory changes and api calling changes_initial

--- GOTApp/scripts/GOTctrls/ctrlGotApp.js
+++ GOTApp/scripts/GOTctrls/ctrlGotApp.js
@@ -1,13 +1,14 @@
-﻿gotApp = angular.module('gotApp', []);
-gotApp.controller('ctrlGotApp', ['$scope', 'ServiceHelper', function ($scope, ServiceHelper) {
+﻿app.controller('ctrlGotApp', ['$scope', 'ServiceHelper', function ($scope, ServiceHelper) {
     window.onload = ServiceHelper.getAllCharacters(
         function (response) {
             if (response != null) {
-                $scope.Characters.push({
-                    Id: value.Id,
-                    Name: value.Name,
-                    gend: value.Code,
-                    ClientSecret: value.Password,
+                $scope.Characters = []; 
+                angular.forEach(response, function (value, key) {
+                    var item = {};
+                    item.Id= value._id;
+                    item.Name= value.name;
+                    item.gender = value.male == true ? "Male" : "Female";
+                    $scope.Characters.push(item);  
                 });
             }
         },
--- GOTApp/scripts/Helpers/ServiceHelper.js
+++ GOTApp/scripts/Helpers/ServiceHelper.js
@@ -1,6 +1,4 @@
-﻿gotApp = angular.module('gotApp', []);
-
-gotApp.factory('ServiceHelper', ['$http', '$scope', function ($http, $scope)
+﻿app.factory('ServiceHelper', ['$http', function ($http)
 {
     var factory = {};
     factory.getAllCharacters = function (successCallBack, failCallBack)
@@ -14,21 +12,26 @@ gotApp.factory('ServiceHelper', ['$http', '$scope', function ($http, $scope)
         request = {
             method: 'GET',
             url: WEB_API_CURRENT_URL,
-            headers: {
-                'Content-Type': AppSetting.ServiceConfig.WEB_API_MEDIA_TYPE
-            }
-        }
+            //headers: {
+            //    'Content-Type': AppSetting.ServiceConfig.WEB_API_MEDIA_TYPE,
+            //    'Authorization': 'bearer SACiin5bkD4VpoLuWqzOYxD5EzfJLaeg4wxIMQ8Isusc7wZWeXWs3S_EF2Lr0qt6zZ1S3lf37eZLRWTm8WdvbQXPE9aoA_VLAxB0bK0DQf2Q-hMjllYNyj4jTrTBmKP6l_sN4wd85ii84QCNyY9IyF5STWZMME7Cy-DqSq9KvsIrRGen02_uSiNsoXmMP6YOE1flXTNvpKVSraqpueqw08L5L6J7Gi8uFY2t1NYSTqSWWLu5Yg4OboBh7eVJ8JlP7zkSBAPFYkULf3Ms-KSjobX0bJycewjMdBG-YNGxV96WjYMgI_T8L-MYe4dhLPrc'
+            //}
+        };
         onSuccess = function (response) {
+            //console.log("success");
             var callbacks = $.Callbacks();
             callbacks.add(successCallBack);
             callbacks.fire(response);
         }
 
         onError = function (response) {
+            //console.log("error");
             var callbacks = $.Callbacks();
             callbacks.add(failCallBack);
             callbacks.fire(response);
         }
+
+        //console.log("jshk");
 
         $http(request)
        .success(onSuccess)
